6c7c89237e80343a8134c41fb8f9ce6b47d63d7b
Eduardo <eduardotortellidudu9@uniplaclages.edu.br>
2024-08-20 16:26:39 -0300

    Implementando e configurando a parte .Web do projeto.

---
+++ LocadoraAutomoveis/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LocadoraAutomoveis.WebApp.Controllers;
+
+public class HomeController : Controller
+{
+    public ActionResult Index()
+    {
+        return View();
+    }
+}
