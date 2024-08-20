dca89a36569eca8bb8d888a91d63483fb15e3c43
Eduardo <eduardotortellidudu9@uniplaclages.edu.br>
2024-08-20 15:23:01 -0300

    Criando os controladores base, entidades base, db context das pastas compartilhado

---
+++ LocadoraAutomoveis.Dominio/Compartilhado/IRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraAutomoveis.Dominio.Compartilhado;
+
+public interface IRepositorio<TEntidade> where TEntidade : EntidadeBase
+{
+    void Inserir(TEntidade entidade);
+    void Editar(TEntidade entidadeAtualizada);
+    void Excluir(TEntidade entidadeParaExcluir);
+    TEntidade? SelecionarPorId(int idSelecionado);
+    List<TEntidade> SelecionarTodos();
+    List<TEntidade> Filtrar(Func<TEntidade, bool> predicate);
+}
+
---
+++ LocadoraAutomoveis.Infra.Orm/Compartilhado/LocadoraAutomoveisDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraAutomoveis.Infra.Orm.Compartilhado;
+
+public class LocadoraAutomoveisDbContext : IdentityDbContext<Usuario, Perfil, int>
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var connectionString = config
+            .GetConnectionString("SqlServer");
+
+        optionsBuilder.UseSqlServer(connectionString);
+
+        optionsBuilder.LogTo(Console.WriteLine).EnableSensitiveDataLogging();
+
+        base.OnConfiguring(optionsBuilder);
+    }
+}
+
