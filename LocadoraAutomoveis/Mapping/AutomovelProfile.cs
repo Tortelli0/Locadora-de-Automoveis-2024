82ba754a09cc226bd7d59dd8f453b691e52add1b
Eduardo <eduardotortellidudu9@uniplaclages.edu.br>
2024-08-26 16:47:57 -0300

    Automovel Profile implementado

--- LocadoraAutomoveis/Mapping/AutomovelProfile.cs
+++ LocadoraAutomoveis/Mapping/AutomovelProfile.cs
@@ -8,8 +8,19 @@ public class AutomovelProfile : Profile
 {
 	public AutomovelProfile()
 	{
+		CreateMap<InserirAutomovelViewModel, Automovel>();
+		CreateMap<EditarAutomovelViewModel, Automovel>();
+
+		CreateMap<Automovel, DetalhesAutomovelViewModel>()
+			.ForMember(
+				dest => dest.GrupoAutomoveis,
+				opt => opt.MapFrom(src => src.GrupoAutomoveis!.Nome)
+			);
+
 		CreateMap<Automovel, ListarAutomovelViewModel>()
 			.ForMember(dest => dest.GrupoAutomoveis,
 				opt => opt.MapFrom(src => src.GrupoAutomoveis.Nome));
+
+		CreateMap<Automovel, EditarAutomovelViewModel>();
 	}
 }
