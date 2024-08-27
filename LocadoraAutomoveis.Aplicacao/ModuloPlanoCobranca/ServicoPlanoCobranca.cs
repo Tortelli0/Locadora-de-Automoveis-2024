8e64adf02eb5dece19f4f6f89d38c9d1d1d864ec
Eduardo <eduardotortellidudu9@uniplaclages.edu.br>
2024-08-27 16:06:28 -0300

    implementado serviço de plano de cobrança

--- LocadoraAutomoveis.Aplicacao/ModuloPlanoCobranca/ServicoPlanoCobranca.cs
+++ LocadoraAutomoveis.Aplicacao/ModuloPlanoCobranca/ServicoPlanoCobranca.cs
@@ -1,10 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentResults;
+using LocadoraAutomoveis.Dominio.ModuloPlanoCobranca;
 
 namespace LocadoraAutomoveis.Aplicacao.ModuloPlanoCobranca;
-internal class ServicoPlanoCobranca
+public class ServicoPlanoCobranca
 {
+	private readonly IRepositorioPlanoCobranca repositoiroPlanoCobranca;
+
+	public ServicoPlanoCobranca(IRepositorioPlanoCobranca repositoiroPlanoCobranca)
+	{
+		this.repositoiroPlanoCobranca = repositoiroPlanoCobranca;
+	}
+
+	public Result<PlanoCobranca> Inserir(PlanoCobranca planoCobranca)
+	{
+		repositoiroPlanoCobranca.Inserir(planoCobranca);
+
+		return Result.Ok(planoCobranca);
+	}
+
+	public Result<PlanoCobranca> Editar(PlanoCobranca planoCobrancaAtualizado)
+	{
+		var planoCobranca = repositoiroPlanoCobranca.SelecionarPorId(planoCobrancaAtualizado.Id);
+
+		if (planoCobranca is null)
+			return Result.Fail("O plano de cobrança não foi encontrado!");
+
+		planoCobranca.GrupoAutomoveisId = planoCobrancaAtualizado.GrupoAutomoveisId;
+		planoCobranca.PrecoDiarioPlanoDiario = planoCobrancaAtualizado.PrecoDiarioPlanoDiario;
+		planoCobranca.PrecoQuilometroPlanoDiario = planoCobrancaAtualizado.PrecoQuilometroPlanoDiario;
+		planoCobranca.QuilometrosDisponiveisPlanoControlado = planoCobrancaAtualizado.QuilometrosDisponiveisPlanoControlado;
+		planoCobranca.PrecoDiarioPlanoControlado = planoCobrancaAtualizado.PrecoDiarioPlanoControlado;
+		planoCobranca.PrecoQuilometroExtrapoladoPlanoControlado = planoCobrancaAtualizado.PrecoQuilometroExtrapoladoPlanoControlado;
+		planoCobranca.PrecoDiarioPlanoLivre = planoCobrancaAtualizado.PrecoDiarioPlanoLivre;
+
+		repositoiroPlanoCobranca.Editar(planoCobranca);
+
+		return Result.Ok(planoCobranca);
+	}
+
+	public Result<PlanoCobranca> Excluir(int planoCobrancaId)
+	{
+		var planoCobranca = repositoiroPlanoCobranca.SelecionarPorId(planoCobrancaId);
+
+		if (planoCobranca is null)
+			return Result.Fail("O plano de cobrança não foi encontrado!");
+
+		repositoiroPlanoCobranca.Excluir(planoCobranca);
+
+		return Result.Ok(planoCobranca);
+	}
 }
