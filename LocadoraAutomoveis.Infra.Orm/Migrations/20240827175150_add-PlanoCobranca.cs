67b8a213a9d1faf0ba3ea54fda1fb001d75254ac
Eduardo <eduardotortellidudu9@uniplaclages.edu.br>
2024-08-27 15:23:08 -0300

    Testes de integração de Plano de cobrança implementado

---
+++ LocadoraAutomoveis.Infra.Orm/Migrations/20240827175150_add-PlanoCobranca.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LocadoraAutomoveis.Infra.Orm.Migrations
+{
+    /// <inheritdoc />
+    public partial class addPlanoCobranca : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TBPlanoCobranca",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GrupoAutomoveisId = table.Column<int>(type: "int", nullable: false),
+                    PrecoDiarioPlanoDiario = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PrecoQuilometroPlanoDiario = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    QuilometrosDisponiveisPlanoControlado = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PrecoDiarioPlanoControlado = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PrecoQuilometroExtrapoladoPlanoControlado = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PrecoDiarioPlanoLivre = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBPlanoCobranca", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TBPlanoCobranca_TBGrupoAutomoveis_GrupoAutomoveisId",
+                        column: x => x.GrupoAutomoveisId,
+                        principalTable: "TBGrupoAutomoveis",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TBPlanoCobranca_GrupoAutomoveisId",
+                table: "TBPlanoCobranca",
+                column: "GrupoAutomoveisId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TBPlanoCobranca");
+        }
+    }
+}
