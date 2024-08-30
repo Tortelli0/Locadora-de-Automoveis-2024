53a937d2f129b169df8cc1d4e9aee9770e6d676d
Eduardo <eduardotortellidudu9@uniplaclages.edu.br>
2024-08-30 14:09:58 -0300

    Testes de Unidade de Cliente implementado

---
+++ LocadoraAutomoveis.Testes.Unidade/ModuloCliente/ClienteTestes.cs
@@ -0,0 +1,35 @@
+﻿using LocadoraAutomoveis.Dominio.ModuloCliente;
+
+namespace LocadoraAutomoveis.Testes.Unidade.ModuloCliente;
+
+[TestClass]
+[TestCategory("Unidade")]
+public class ClienteTestes
+{
+	[TestMethod]
+	public void Deve_Criar_Instancia_Valida()
+	{
+		var cliente = new Cliente("Jose", "jose@gmail.com", "99999-9999", TipoCadastroClienteEnum.CPF, "000.000.000-00", "SC", "Lages", "Centro", "Correia Pinto", "55");
+
+		var erros = cliente.Validar();
+
+		Assert.AreEqual(0, erros.Count);
+	}
+
+	[TestMethod]
+	public void Deve_Criar_Instancia_Com_Erro()
+	{
+		var cliente = new Cliente("Jose", "jose@gmail.com", "99999-9999", TipoCadastroClienteEnum.CPF, "000.000.000-00", "SC", "", "", "Correia Pinto", "55");
+
+		var erros = cliente.Validar();
+
+		List<string> errosEsperados = 
+		[
+			"A cidade é obrigatória",
+			"O bairro é obrigatório"
+		];
+
+		Assert.AreEqual(errosEsperados.Count, erros.Count);
+		CollectionAssert.AreEqual(errosEsperados, erros);
+	}
+}
