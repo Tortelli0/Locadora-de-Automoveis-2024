a5ebe44dd617162d11fc267d060c2fe9b28a2e94
Eduardo <eduardotortellidudu9@uniplaclages.edu.br>
2024-08-30 15:14:22 -0300

    testes de integração de cliente implementado

--- LocadoraAutomoveis.Testes.Integracao/ModuloCliente/RepositorioClienteEmOrmTestes.cs
+++ LocadoraAutomoveis.Testes.Integracao/ModuloCliente/RepositorioClienteEmOrmTestes.cs
@@ -1,9 +1,63 @@
-﻿namespace LocadoraAutomoveis.Testes.Integracao.ModuloCliente;
+﻿using FizzWare.NBuilder;
+using LocadoraAutomoveis.Dominio.ModuloCliente;
+using LocadoraAutomoveis.Testes.Integracao.Compartilhado;
+
+namespace LocadoraAutomoveis.Testes.Integracao.ModuloCliente;
 
 [TestClass]
 [TestCategory("Integração")]
-public class RepositorioClienteEmOrmTestes
+public class RepositorioClienteEmOrmTestes : RepositorioEmOrmTestesBase
 {
-	[TestInitialize]
+	[TestMethod]
+	public void Deve_Inserir_Cliente()
+	{
+		var cliente = Builder<Cliente>
+			.CreateNew()
+			.With(c => c.Id = 0)
+			.Build();
 
+		repositorioCliente.Inserir(cliente);
+
+		var clienteSelecionado = repositorioCliente.SelecionarPorId(cliente.Id);
+
+		Assert.IsNotNull(clienteSelecionado);
+		Assert.AreEqual(cliente, clienteSelecionado);
+	}
+
+	[TestMethod]
+	public void Deve_Editar_Cliente()
+	{
+		var cliente = Builder<Cliente>
+			.CreateNew()
+			.With(c => c.Id = 0)
+			.Persist();
+
+		cliente.Nome = "Nome Atualizado";
+		cliente.Email = "novoemail@dominio.com";
+
+		repositorioCliente.Editar(cliente);
+
+		var clienteSelecionado = repositorioCliente.SelecionarPorId(cliente.Id);
+
+		Assert.IsNotNull(clienteSelecionado);
+		Assert.AreEqual(cliente, clienteSelecionado);
+	}
+
+	[TestMethod]
+	public void Deve_Excluir_Cliente()
+	{
+		var cliente = Builder<Cliente>
+			.CreateNew()
+			.With(c => c.Id = 0)
+			.Persist();
+
+		repositorioCliente.Excluir(cliente);
+
+		var clienteSelecionado = repositorioCliente.SelecionarPorId(cliente.Id);
+
+		var clientes = repositorioCliente.SelecionarTodos();
+
+		Assert.IsNull(clienteSelecionado);
+		Assert.AreEqual(0, clientes.Count);
+	}
 }
