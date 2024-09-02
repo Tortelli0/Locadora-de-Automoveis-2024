fc9eba4b17c6569b0a84b4ad4c6f14222d8bae59
Eduardo <eduardotortellidudu9@uniplaclages.edu.br>
2024-09-02 15:25:42 -0300

    testes de integração de condutor implementado

--- LocadoraAutomoveis.Testes.Integracao/Compartilhado/RepositorioEmOrmTestesBase.cs
+++ LocadoraAutomoveis.Testes.Integracao/Compartilhado/RepositorioEmOrmTestesBase.cs
@@ -1,12 +1,14 @@
 ﻿using FizzWare.NBuilder;
 using LocadoraAutomoveis.Dominio.ModuloAutomoveis;
 using LocadoraAutomoveis.Dominio.ModuloCliente;
+using LocadoraAutomoveis.Dominio.ModuloCondutor;
 using LocadoraAutomoveis.Dominio.ModuloGrupoAutomoveis;
 using LocadoraAutomoveis.Dominio.ModuloPlanoCobranca;
 using LocadoraAutomoveis.Dominio.ModuloTaxa;
 using LocadoraAutomoveis.Infra.Orm.Compartilhado;
 using LocadoraAutomoveis.Infra.Orm.ModuloAutomoveis;
 using LocadoraAutomoveis.Infra.Orm.ModuloCliente;
+using LocadoraAutomoveis.Infra.Orm.ModuloCondutor;
 using LocadoraAutomoveis.Infra.Orm.ModuloGrupoAutomoveis;
 using LocadoraAutomoveis.Infra.Orm.ModuloPlanoCobranca;
 using LocadoraAutomoveis.Infra.Orm.ModuloTaxaEmOrm;
@@ -21,6 +23,7 @@ public abstract class RepositorioEmOrmTestesBase
 	protected RepositorioAutomoveisEmOrm repositorioAutomovel;
 	protected RepositorioGrupoAutomoveisEmOrm repositorioGrupo;
 	protected RepositorioPlanoCobrancaEmOrm repositorioPlano;
+    protected RepositorioCondutorEmOrm repositorioCondutor;
 
 	[TestInitialize]
 	public void Inicializar()
@@ -32,6 +35,7 @@ public abstract class RepositorioEmOrmTestesBase
 		dbContext.Clientes.RemoveRange(dbContext.Clientes);
 		dbContext.Automoveis.RemoveRange(dbContext.Automoveis);
 		dbContext.GrupoAutomoveis.RemoveRange(dbContext.GrupoAutomoveis);
+		dbContext.Condutores.RemoveRange(dbContext.Condutores);
 
 		dbContext.SaveChanges();
 
@@ -40,11 +44,13 @@ public abstract class RepositorioEmOrmTestesBase
 		repositorioCliente = new RepositorioClienteEmOrm(dbContext);
 		repositorioAutomovel = new RepositorioAutomoveisEmOrm(dbContext);
 		repositorioGrupo = new RepositorioGrupoAutomoveisEmOrm(dbContext);
+        repositorioCondutor = new RepositorioCondutorEmOrm(dbContext);
 
 		BuilderSetup.SetCreatePersistenceMethod<Taxa>(repositorioTaxa.Inserir);
 		BuilderSetup.SetCreatePersistenceMethod<PlanoCobranca>(repositorioPlano.Inserir);
 		BuilderSetup.SetCreatePersistenceMethod<Cliente>(repositorioCliente.Inserir);
 		BuilderSetup.SetCreatePersistenceMethod<Automovel>(repositorioAutomovel.Inserir);
 		BuilderSetup.SetCreatePersistenceMethod<GrupoAutomoveis>(repositorioGrupo.Inserir);
+		BuilderSetup.SetCreatePersistenceMethod<Condutor>(repositorioCondutor.Inserir);
 	}
 }
---
+++ LocadoraAutomoveis.Testes.Integracao/ModuloCondutor/RepositorioCondutorEmOrmTestes.cs
@@ -0,0 +1,83 @@
+﻿using FizzWare.NBuilder;
+using LocadoraAutomoveis.Dominio.ModuloCliente;
+using LocadoraAutomoveis.Dominio.ModuloCondutor;
+using LocadoraAutomoveis.Testes.Integracao.Compartilhado;
+using NuGet.Frameworks;
+
+namespace LocadoraAutomoveis.Testes.Integracao.ModuloCondutor;
+
+[TestClass]
+[TestCategory("Integração")]
+public class RepositorioCondutorEmOrmTestes : RepositorioEmOrmTestesBase
+{
+    [TestMethod]
+    public void Deve_Inserir_Condutor()
+    {
+        var cliente = Builder<Cliente>
+            .CreateNew()
+            .With(c => c.Id = 0)
+            .Persist();
+
+        var condutor = Builder<Condutor>
+            .CreateNew()
+            .With(c => c.Id = 0)
+            .With(c => c.ClienteId = cliente.Id)
+            .Build();
+
+        repositorioCondutor.Inserir(condutor);
+
+        var condutorSelecionado = repositorioCondutor.SelecionarPorId(condutor.Id);
+
+        Assert.IsNotNull(condutorSelecionado);
+        Assert.AreEqual(condutor, condutorSelecionado);
+    }
+
+    [TestMethod]
+    public void Deve_Editar_Condutor()
+    {
+        var cliente = Builder<Cliente>
+            .CreateNew()
+            .With(c => c.Id = 0)
+            .Persist();
+
+        var condutor = Builder<Condutor>
+            .CreateNew()
+            .With(c => c.Id = 0)
+            .With(c => c.ClienteId = cliente.Id)
+            .Persist();
+
+        condutor.Nome = "Messias";
+        condutor.Telefone = "49 90000-0000";
+
+        repositorioCondutor.Editar(condutor);
+
+        var condutorSelecionado = repositorioCondutor.SelecionarPorId(condutor.Id);
+
+        Assert.IsNotNull(condutorSelecionado);
+        Assert.AreEqual(condutor, condutorSelecionado);
+    }
+
+    [TestMethod]
+    public void Deve_Excluir_Condutor()
+    {
+        var cliente = Builder<Cliente>
+            .CreateNew()
+            .With(c => c.Id = 0)
+            .Persist();
+
+        var condutor = Builder<Condutor>
+            .CreateNew()
+            .With(c => c.Id = 0)
+            .With(c => c.ClienteId = cliente.Id)
+            .Persist();
+
+        repositorioCondutor.Excluir(condutor);
+
+        var condutorSelecionado = repositorioCondutor.SelecionarPorId(condutor.Id);
+
+        var condutores = repositorioCondutor.SelecionarTodos();
+
+        Assert.IsNull(condutorSelecionado);
+        Assert.AreEqual(0, condutores.Count);
+    }
+}
