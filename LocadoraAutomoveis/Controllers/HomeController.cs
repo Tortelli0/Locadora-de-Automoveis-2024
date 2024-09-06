7d01a7b7bca62b0954404acd1ef8ed164e44c6c7
Eduardo <eduardotortellidudu9@uniplaclages.edu.br>
2024-09-06 18:52:19 -0300

    Pagina home refatorada

--- LocadoraAutomoveis/Controllers/HomeController.cs
+++ LocadoraAutomoveis/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LocadoraAutomoveis.WebApp.Controllers;
 
+[Authorize(Roles = "Empresa,Funcionario")]
 public class HomeController : Controller
 {
     public ActionResult Index()
